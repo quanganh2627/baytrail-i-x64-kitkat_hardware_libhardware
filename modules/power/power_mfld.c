8078987f650ec0ceeb51caa76899befc63db95c0
Ashish K <ashish.k@intel.com>
2013-01-16 00:31:48 +0530

    PM: Power HAL adaptation for CLV-MFLD
    BZ: 69850
    
    This patch differentiates Power HAL implementations across platforms.
    
    Change-Id: I4a4d1d32bf31b76eb2063e4db1219e8dcefb9796
    Signed-off-by: Ashish K <ashish.k@intel.com>
    Reviewed-on: http://android.intel.com:8080/86730
    Reviewed-by: Kumar P, Mahesh <mahesh.kumar.p@intel.com>
    Reviewed-by: Rudramuni, Vishwesh M <vishwesh.m.rudramuni@intel.com>
    Tested-by: Cuesta, FernandX <fernandx.cuesta@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

---
+++ modules/power/power_mfld.c
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define LOG_TAG "Intel PowerHAL"
+#include <utils/Log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/power.h>
+
+#define TIMER_RATE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/timer_rate"
+#define UP_THRESHOLD_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load"
+#define BOOST_PULSE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
+
+struct intel_power_module{
+	struct power_module container;
+};
+
+static void sysfs_write(char *path, char *s)
+{
+    char buf[80];
+    int len;
+    int fd = open(path, O_WRONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+        return;
+    }
+
+    len = write(fd, s, strlen(s));
+    if (len < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+    }
+
+    close(fd);
+}
+
+static void intel_power_init(struct power_module *module)
+{
+	ALOGW("**Intel Power HAL initialisation**\n");
+/*initialization*/
+	sysfs_write(TIMER_RATE_SYSFS,"100000");
+	sysfs_write(UP_THRESHOLD_SYSFS,"70");
+}
+
+static void intel_power_set_interactive(struct power_module *module, int on)
+{
+}
+
+static void intel_power_hint(struct power_module *module, power_hint_t hint,
+                       void *data) {
+
+	struct intel_power_module *intel_module = (struct intel_power_module*)module;
+	switch (hint) {
+		case POWER_HINT_INTERACTION:
+			sysfs_write(BOOST_PULSE_SYSFS,"1");
+		break;
+    default:
+        break;
+    }
+}
+
+static struct hw_module_methods_t power_module_methods = {
+    .open = NULL,
+};
+
+
+
+struct intel_power_module HAL_MODULE_INFO_SYM = {
+    container:{
+			common: {
+			tag: HARDWARE_MODULE_TAG,
+			module_api_version: POWER_MODULE_API_VERSION_0_2,
+			hal_api_version: HARDWARE_HAL_API_VERSION,
+			id: POWER_HARDWARE_MODULE_ID,
+			name: "Intel Power HAL",
+			author: "Power Management Team",
+			methods: &power_module_methods,
+	},
+    init: intel_power_init,
+    setInteractive: intel_power_set_interactive,
+    powerHint: intel_power_hint,
+    },
+};
