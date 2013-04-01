f4c598641ba3c53902f93993121257d21887c328
Mahesh.kumar.P <mahesh.kumar.p@intel.com>
2013-04-01 15:33:47 +0530

    PowerHAL: Optimising power HAL touch-vsync boost
    BZ: 96448
    
    The VSYNC events on MR1.1 is more frequent as compared to the
    MR1 JB release. The TOUCH and VSYNC hints are both triggered
    from the system_server context. The POwer HAL has been enhanced
    accordingly.
    The Power HAL now considers
    1) short touches
    2) scrolling
    3) long touches
    
    The vsync boost has been implemented after the user releases the
    touch.
    
    Change-Id: Ie625c97b89113901a5c515d72549bbc69450d308
    Signed-off-by: Kumar Mahesh <mahesh.kumar.p@intel.com>
    Reviewed-on: http://android.intel.com:8080/99256
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Mansoor, Illyas <illyas.mansoor@intel.com>
    Tested-by: Cuesta, Fernand <fernand.cuesta@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- modules/power/power_mfld.c
+++ modules/power/power_mfld.c
@@ -18,6 +18,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include<time.h>
 
 #define LOG_TAG "Intel PowerHAL"
 #include <utils/Log.h>
@@ -26,14 +27,39 @@
 #include <hardware/power.h>
 
 #define TIMER_RATE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/timer_rate"
-#define UP_THRESHOLD_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load"
 #define BOOST_PULSE_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define TOUCH_EVENT_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/touch_event"
-#define VSYNC_COUNT_SYSFS	"/sys/devices/system/cpu/cpufreq/interactive/vsync_count"
-#define VSYNC_DEC_SYSFS		"/sys/devices/system/cpu/cpufreq/interactive/vsync_dec"
+
+/*
+ * This parameter is to identify continuous touch/scroll events.
+ * Any two touch hints received between a 20 interval ms is
+ * considered as a scroll event.
+ */
+#define SHORT_TOUCH_TIME 20
+
+/*
+ * This parameter is to identify first touch events.
+ * Any two touch hints received after 100 ms is considered as
+ * a first touch event.
+ */
+#define LONG_TOUCH_TIME 100
+
+/*
+ * This parameter defines the number of vsync boost to be
+ * done after the finger release event.
+ */
+#define VSYNC_BOOST_COUNT 4
+
+/*
+ * This parameter defines the time between a touch and a vsync
+ * hint. the time if is > 30 ms, we do a vsync boost.
+ */
+#define VSYNC_TOCUH_TIME 30
 
 struct intel_power_module{
 	struct power_module container;
+	int touchboost_disable;
+	int timer_set;
+	int vsync_boost;
 };
 
 static void sysfs_write(char *path, char *s)
@@ -89,45 +115,69 @@ static void intel_power_set_interactive(struct power_module *module, int on)
 
 static void intel_power_hint(struct power_module *module, power_hint_t hint,
                        void *data) {
+	struct intel_power_module *intel = (struct intel_power_module *) module;
 	char sysfs_val[80];
+	static clock_t curr_time, prev_time , vsync_time;
+	float diff;
+	static int vsync_count;
+	static int consecutive_touch_int;
 
 	switch (hint) {
 		case POWER_HINT_INTERACTION:
-			if (sysfs_read(TOUCH_EVENT_SYSFS,sysfs_val,sizeof(sysfs_val)) == -1) {
-				ALOGE("Error in reading touch hint\n");
-			}
-			if((atoi(sysfs_val)) == 0) {
-				sysfs_write(TOUCH_EVENT_SYSFS,"1");
-				sysfs_write(BOOST_PULSE_SYSFS,"1");
-				sysfs_write(VSYNC_COUNT_SYSFS,"4");
-				sysfs_write(TIMER_RATE_SYSFS,"20000");
-			}
+		curr_time = clock();
+		diff = (((float)curr_time - (float)prev_time) / CLOCKS_PER_SEC ) * 1000;
+		prev_time = curr_time;
+		if(diff < SHORT_TOUCH_TIME)
+			consecutive_touch_int ++;
+		else if (diff > LONG_TOUCH_TIME) {
+			intel->vsync_boost = 0;
+			intel->timer_set = 0;
+			intel->touchboost_disable = 0;
+			vsync_count = 0;
+			consecutive_touch_int = 0;
+			sysfs_write(TIMER_RATE_SYSFS,"100000");
+		}
+		/* Simple touch: timer rate need not be changed here */
+		if((diff < SHORT_TOUCH_TIME) && (intel->touchboost_disable == 0)
+											&& (consecutive_touch_int > 4))
+			intel->touchboost_disable = 1;
+		/*
+		 *Scrolling: timer rate reduced to increase sensitivity. No more touch
+		 *boost after this
+		*/
+		if((intel->touchboost_disable == 1) && (consecutive_touch_int > 15)
+												&& (intel->timer_set == 0)) {
+			intel->timer_set = 1;
+			sysfs_write(TIMER_RATE_SYSFS,"20000");
+		}
+		if (!intel->touchboost_disable) {
+			sysfs_write(BOOST_PULSE_SYSFS,"1");
+		}
 		break;
-
 		case POWER_HINT_VSYNC:
-			if (sysfs_read(VSYNC_COUNT_SYSFS,sysfs_val,sizeof(sysfs_val)) == -1) {
-				ALOGE("Error in reading vsync count\n");
-			}
 
-			if((data == 1) && ((atoi(sysfs_val)) > 0)) {
-				if((atoi(sysfs_val)) < 4)
-					sysfs_write(BOOST_PULSE_SYSFS,"1");
-				sysfs_write(VSYNC_DEC_SYSFS,"1");
+		if(intel->touchboost_disable == 1) {
+			vsync_time = clock();
+			diff = (((float)vsync_time - (float)curr_time) / CLOCKS_PER_SEC ) * 1000;
+			if(diff > VSYNC_TOCUH_TIME) {
+				sysfs_write(TIMER_RATE_SYSFS,"100000");
+				intel->timer_set = 0;
+				intel->vsync_boost = 1;
+				intel->touchboost_disable = 0;
+				vsync_count = VSYNC_BOOST_COUNT;
 			}
-
-			if (sysfs_read(TOUCH_EVENT_SYSFS,sysfs_val,sizeof(sysfs_val)) == -1) {
-				ALOGE("Error in reading vsync hint\n");
-			}
-
-			if((data == 0) && ((atoi(sysfs_val)) == 1)) {
-			    sysfs_write(TIMER_RATE_SYSFS,"100000");
+		}
+		if(intel->vsync_boost) {
+			if((data == 1) && (vsync_count > 0)) {
 				sysfs_write(BOOST_PULSE_SYSFS,"1");
-				sysfs_write(TOUCH_EVENT_SYSFS,"0");
+				vsync_count -- ;
+				if(vsync_count == 0)
+					intel->vsync_boost = 0;
 			}
+		}
 		break;
-
-	    default:
-        break;
+		default:
+		break;
     }
 }
 
@@ -152,4 +202,7 @@ struct intel_power_module HAL_MODULE_INFO_SYM = {
     setInteractive: intel_power_set_interactive,
     powerHint: intel_power_hint,
     },
+	touchboost_disable: 0,
+	timer_set: 0,
+	vsync_boost: 0,
 };
